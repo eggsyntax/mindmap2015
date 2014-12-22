7c0a6be01d02369ca6e5504b21ec0afb9edfdafc
George Kierstein <gateway.spectacle@gmail.com>
2014-12-22 18:28:16 -0500

    Cleaned up init

--- repo-config/init.py
+++ repo-config/init.py
@@ -5,21 +5,52 @@
 #
 # Note: Any extensions are stripped off before copying
 #
+# Allow the script to be run from either the top-level repo
+# directory *or* the config directory itself
+#
 import sys, os, shutil
 
-# we *should* be invoked from the repo-config directory
 cwd = os.getcwd()
-repoDir = os.path.dirname(cwd)
+
+if ('repo-config' in os.listdir(cwd)):
+  repoDir = cwd
+  initDir = os.path.join(cwd, 'repo-config')
+else:
+  repoDir = os.path.dirname(cwd)
+  initDir = cwd
+
 gitDir = os.path.join(repoDir, '.git')
 hookDir = os.path.join(gitDir, 'hooks')
+print 'init.py> Installing to: ', hookDir
 
-ignore = ['README', 'init.py']
+# List of all available hooks
+#
+# http://git-scm.com/docs/githooks
+#
+hooks = ['applypatch-msg'
+        , 'pre-applypatch'
+        , 'post-applypatch'
+        , 'pre-commit'
+        , 'prepare-commit-msg'
+        , 'commit-msg'
+        , 'post-commit'
+        , 'pre-rebase'
+        , 'post-checkout'
+        , 'post-merge'
+        , 'pre-push'
+        , 'pre-receive'
+        , 'update'
+        , 'post-receive'
+        , 'post-update'
+        , 'pre-auto-gc'
+        , 'post-rewrite']
 
-for script in os.listdir(cwd):
-  if (not script in ignore):
-    cp = os.path.splitext(script)[0]
+os.chdir(initDir)
+for script in os.listdir(initDir):
+  cp = os.path.splitext(script)[0]
+  if (cp in hooks):
     dest = os.path.join(hookDir, cp)
-    msg = 'init> Copying (' + script +') to: ' + dest
+    msg = 'init> Copying (' + script +') to ' + cp
     print msg
     shutil.copy(script, dest)
 

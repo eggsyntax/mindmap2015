26edf1059bbc5b288bd2800a75133deee7f351fd
Egg Syntax <egg@novonon.com>
2015-02-03 20:27:22 -0500

    Add C2 dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.keminglabs/c2 "0.2.3"]
                  [org.clojure/tools.namespace "0.2.9"]]
   :main ^:skip-aot mindmap.core
   :target-path "target/%s"

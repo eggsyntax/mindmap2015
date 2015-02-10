305acc9b8f227891fa332173fd13e2c826bc6215
Egg Syntax <egg@novonon.com>
2015-02-10 08:15:32 -0500

    Stubs for serialization

---
+++ test/mindmap/test_serialize.clj
@@ -0,0 +1,11 @@
+(ns mindmap.test-serialize
+  (:require [clojure.test :refer :all]
+            ))
+
+; TODO Working on getting basic testing working!
+(testing "Basic math is sane?"
+  (with-test
+    (is (= 4 (+ 2 2)))))
+
+
+(run-tests 'mindmap.test-serialize)

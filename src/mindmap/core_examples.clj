6f55a69a96e0bf0fcc8d6217989f375e4759b2a9
George Kierstein <gateway.spectacle@gmail.com>
2015-02-17 22:40:05 -0500

    Cleaned up core_examples to be consistent with current interface

--- src/mindmap/core_examples.clj
+++ src/mindmap/core_examples.clj
@@ -16,42 +16,29 @@
 (demo @hypermap)
 
 (get-mm @hypermap (:head-pointer @hypermap))
+(def node1 (get-cur @hypermap))
 
-(demo (get-cur @hypermap))
-
+; What is this for ?
 (demo (:maps @hypermap))
 (demo (assoc-in @hypermap [:maps 1234] {:foo :bar}))
 
-(demo (def anode (mm/create-entity {:title "Second node"})))
-(demo (swap! hypermap add-node anode))
-(demo (add-node @hypermap anode))
-(demo @hypermap)
+(demo (swap! hypermap add-node {:title "Node 2"}))
+(def node2 (get-cur @hypermap))
+(demo node2)
 
-(println (hash anode))
-(def anothernode (mm/create-entity {:title "Third node"}))
-(println (hash anothernode))
-(demo anothernode)
+(demo (swap! hypermap add-node {:title "Node 3"}))
+(def node3 (get-cur @hypermap))
+(demo node3)
 
-(demo (swap! hypermap add-node anothernode))
-(demo @hypermap)
-
-(demo (swap! hypermap add-edge firstnode anode {:title "Edge 1" :type :child}))
-(demo (swap! hypermap add-edge anode anothernode {:title "Edge 2" :type :child}))
+(demo (swap! hypermap add-edge node1 node2 {:title "Edge 1" :type :child}))
+(demo (swap! hypermap add-edge node1 node3 {:title "Edge 2" :type :child}))
 
 ; We can add a second edge between the same pair of nodes (some type of edge that I just made up)
-(demo (swap! hypermap add-edge anode anothernode
-             {:title "Edge 3" :type :file-contains}))
-(demo (swap! hypermap add-edge anode firstnode {:title "Edge 4" :type :child}))
+(demo (swap! hypermap add-edge node1 node2 {:title "Edge 3" :type :file-contains}))
+(demo (swap! hypermap add-edge node1 node3 {:title "Edge 4" :type :child}))
 
 (print-head @hypermap)
-(get-cur @hypermap)
-
-(demo (mm/add-node-returning-mm-and-node (get-head @hypermap) {:title "TEMP NODE"}))
-(mm/get-edges @hypermap [67 69])
-
-(get-head @hypermap)
-((:nodes (get-head @hypermap)) 101)
-(mm/get-node (get-head @hypermap) 101)
+(demo (get-cur @hypermap))
 
 ; apply-filters example
 (def f-list [even?
@@ -60,24 +47,23 @@
 (apply-filters f-list (range 11))
 (apply-filters nil (range 11))
 
-(demo (edges-from @hypermap anode))
+(demo (edges-from @hypermap node2))
 
-(def ef (edges-from @hypermap anode))
+(def ef (edges-from @hypermap node2))
 (:nodes (get-head @hypermap))
-; (:type (:id anode) ((:nodes (get-head @hypermap)) ))
 (def edges (:edges (get-head @hypermap)))
 (println edges)
 
-(mm/get-node (get-head @hypermap) (:id anode))
+(mm/get-node (get-head @hypermap) (:id node3))
 (def child-filter #(= (:type (mm/get-node (get-head @hypermap) %)) :child))
 (def test-filter #(= (:type %) :child))
 
 (print-head @hypermap)
-(demo (edges-from @hypermap anode))
+(demo (edges-from @hypermap node1))
 (demo (apply-filters
         [child-filter]
-        (edges-from @hypermap anode)))
+        (edges-from @hypermap node1)))
 
 ; For a single filter, we could just do "(filter my-filter" instead of "apply-filters [my-filter]"
-(demo (apply-filters [test-filter] (edges-from @hypermap anode)))
+(demo (apply-filters [test-filter] (edges-from @hypermap node1)))
 

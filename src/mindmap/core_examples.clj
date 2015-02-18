91361d70c3200fc38a20b2d720c61b15e35ae69a
Egg Syntax <egg@novonon.com>
2015-02-18 07:46:02 -0500

    Bit of cleanup, make core_examples produce a richer example

--- src/mindmap/core_examples.clj
+++ src/mindmap/core_examples.clj
@@ -3,6 +3,8 @@
         [mindmap.util])
   (:require [mindmap.mm :as mm]))
 
+(reset! debug-mode true)
+;(r!) ; Refresh REPL before run
 
 ; Just gives some examples of how the core functions work.
 ; Warning! Anything in here with hardcoded numbers won't succeed across
@@ -31,10 +33,10 @@
 (demo node3)
 
 (demo (swap! hypermap add-edge node1 node2 {:title "Edge 1" :type :child}))
-(demo (swap! hypermap add-edge node1 node3 {:title "Edge 2" :type :child}))
+(demo (swap! hypermap add-edge node2 node3 {:title "Edge 2" :type :child}))
 
 ; We can add a second edge between the same pair of nodes (some type of edge that I just made up)
-(demo (swap! hypermap add-edge node1 node2 {:title "Edge 3" :type :file-contains}))
+(demo (swap! hypermap add-edge node2 node3 {:title "Edge 3" :type :file-contains}))
 (demo (swap! hypermap add-edge node1 node3 {:title "Edge 4" :type :child}))
 
 (print-head @hypermap)

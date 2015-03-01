7c3e17bfbd268937f9af01ca78e96a63bc522ceb
Egg Syntax <egg@novonon.com>
2015-03-01 08:57:01 -0500

    Working on as-tree

--- src/mindmap/as_tree.clj
+++ src/mindmap/as_tree.clj
@@ -1,8 +1,6 @@
 (ns mindmap.as-tree
   (:use [mindmap.mm]
-        [mindmap.util :as ut]
-
-        ))
+        [mindmap.util :as ut]))
 
 "Functions for returning tree views of mindmaps"
 
@@ -20,8 +18,32 @@
             (str "This freaky node has too many parents. " node ":\n\t" parents))
     (first parents)))
 
-(defn subtree
-  "Return the subtree whose root is 'node', to a depth of 'depth'"
-  [mm node depth]
+(defn make-node-edge-map
+  "Return a map from node to (edges). Allows fast downward navigation. Runs
+  in O(E) with E the number of edges."
+  [mm]
+  (let [edges (:edges mm)
+        add-to-map (fn [cur-map edge]
+                     (update-in cur-map [(:origin-id edge)] conj edge))]
+    (reduce add-to-map {} edges)))
 
-  )
+(ut/demo (def rmm (rand-mm :num-nodes 8 :seed -1)))
+(ut/ppprint (make-node-edge-map rmm))
+
+(defn subtree
+  "Return the subtree whose root is 'node', to a depth of 'depth'. Pass depth
+  of nil to return entire tree. Throws error if resulting subtree would contain
+  circularity."
+  ; Use set of visited nodes to detect circularity
+  [mm node depth]
+  (let [edges (:edges mm)
+        init-tree '(node)
+        node-edge-map (make-node-edge-map mm)
+        ]
+    (loop [tree init-tree
+           levels depth
+           visited #{}
+           cur node
+           ]
+      ;
+      )))

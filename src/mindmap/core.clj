c89565f9256fbe68f2439f9f716a8f5374b0b3a2
George Kierstein <gateway.spectacle@gmail.com>
2015-03-02 20:34:47 -0500

    Refactored default-hypertree to pull out hypertree creation from mindmap creation. Added random-hypertree

--- src/mindmap/core.clj
+++ src/mindmap/core.clj
@@ -1,5 +1,5 @@
 (ns mindmap.core
-  (:require [mindmap.console.core :as console])
+;  (:require [mindmap.console.core :as console])
   (:require [clojure.tools.namespace.repl :only  (refresh)]))
 
 (defn -main [& args]
@@ -8,4 +8,6 @@
                       (args ":swing") :swing
                       (args ":text")  :text
                       :else           :auto) ]
-      (console/main screen-type true)))
+     ; (console/main screen-type true)
+      
+      ))
